18ac1388a17336b5e54d06765511cc9e5dcf9710
Yujin Boby <admin@hostonnet.com>
2015-11-26 23:04:00 +0530

    added csf install instructions.

---
+++ csf.sh
@@ -0,0 +1,12 @@
+#!/bin/badh
+
+cd /usr/local/src
+wget http://www.configserver.com/free/csf.tgz
+tar -xzf csf.tgz
+cd csf
+sh install.sh
+
+rm -f /etc/csf/csf.conf
+cd /etc/csf
+wget https://github.com/HostOnNet/server-setup/raw/master/data/csf.conf
+
