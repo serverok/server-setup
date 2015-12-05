cc6fd3c6f0f9f70f3c3327cfaac6e3944a9f3d4d
Yujin Boby <admin@hostonnet.com>
2015-12-05 23:27:47 +0530

    git installed

--- ubuntu/1-basic-tools.sh
+++ ubuntu/1-basic-tools.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+locale-gen en_US.UTF-8
+dpkg-reconfigure locales
+
 apt-get update
 apt-get -y upgrade
-apt-get -y install wget curl nmap whois vim screen
+apt-get -y install wget curl nmap whois vim screen git
 
