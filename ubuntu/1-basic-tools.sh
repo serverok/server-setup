891309453250a0d7396f9eee5cd747562352da52
Yujin Boby <admin@hostonnet.com>
2015-12-21 12:28:29 +0530

    updated ubuntu basic tools install

--- ubuntu/1-basic-tools.sh
+++ ubuntu/1-basic-tools.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # https://blog.hostonnet.com/ubuntu-14-04-openvz-locale-cannot-set-lc_all-to-default-locale-no-such-file-or-directory
+echo "LC_ALL=en_US.UTF-8" >> /etc/environment
+echo "LANG=en_US.UTF-8" >> /etc/environment
 export LANGUAGE=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
