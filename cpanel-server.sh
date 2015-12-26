d1479d2f1a363b806c042bf6c2c604796354616d
Yujin Boby <admin@hostonnet.com>
2015-12-26 09:35:33 +0530

    updated cpanel-server.sh file, include cpanel.config updation

--- cpanel-server.sh
+++ cpanel-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat /root/cpanel3-skel/.cpanel-logs
+mkdir /root/cpanel3-skel/
 echo "archive-logs=1" > /root/cpanel3-skel/.cpanel-logs
 echo "remove-old-archived-logs=1" >> /root/cpanel3-skel/.cpanel-logs
 
@@ -10,4 +10,9 @@ echo "remove-old-archived-logs=1" >> /root/cpanel3-skel/.cpanel-logs
 # Copy EasyApache3 Profile
 rm -f /var/cpanel/easy/apache/profile/custom/hostonnet.yaml
 /bin/cp data/hostonnet.yaml /var/cpanel/easy/apache/profile/custom/hostonnet.yaml
+
+# Copy cpanel settings
+rm -f /var/cpanel/cpanel.config
+cp data/cpanel.config /var/cpanel/
+/usr/local/cpanel/whostmgr/bin/whostmgr2 -updatetweaksettings
 
