1d0f7c311f7ab8a58117085ab7e8285f0c3664d4
Yujin Boby <admin@hostonnet.com>
2015-12-26 12:16:08 +0530

    added ssh-keygen and secure-sysctl.sh

--- secure-sysctl.sh
+++ secure-sysctl.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sysctl --all | grep ptrace
+# http://docs.cloudlinux.com/index.html?ptrace_block.html
 echo "kernel.user_ptrace = 0" >> /etc/sysctl.conf
 sysctl -p
 sysctl --all | grep ptrace
