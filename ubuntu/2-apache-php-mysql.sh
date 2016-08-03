3307c01171c742848bda1e7ccf62650a947e241d
Yujin Boby <admin@hostonnet.com>
2016-08-03 22:16:12 +0530

    ubuntu install instruction updated

--- ubuntu/2-apache-php-mysql.sh
+++ ubuntu/2-apache-php-mysql.sh
@@ -4,9 +4,9 @@ apt-get -y install apache2
 update-rc.d apache2 enable
 a2enmod rewrite
 
-apt-get -y install php5 php5-cli php5-curl php5-gd php5-mysql php5-imagick php5-imap php5-mcrypt php5-json
-php5enmod mcrypt
+apt-get -y install php php-cli php-curl php-gd php-mysql php-imagick php-imap php-mcrypt php-json
+phpenmod mcrypt
 
-apt-get -y install mariadb-client-5.5 mariadb-server-5.5
+apt-get -y install mariadb-client mariadb-server
 update-rc.d mysql enable
 
