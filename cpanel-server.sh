398b755a3ce0cb9b399a248826bd927e29fe77f1
Yujin Boby <admin@hostonnet.com>
2016-08-16 23:03:43 +0530

    removed easyapache 3 profile. Go with EA4 on all new servers

--- cpanel-server.sh
+++ cpanel-server.sh
@@ -6,13 +6,3 @@ echo "remove-old-archived-logs=1" >> /root/cpanel3-skel/.cpanel-logs
 
 # disable mail from mailer-daemon
 /bin/sed -i "s/mailer-daemon:\tpostmaster/mailer-daemon: \/dev\/null/g" /etc/aliases
-
-# Copy EasyApache3 Profile
-rm -f /var/cpanel/easy/apache/profile/custom/hostonnet.yaml
-/bin/cp data/hostonnet.yaml /var/cpanel/easy/apache/profile/custom/hostonnet.yaml
-
-# Copy cpanel settings
-rm -f /var/cpanel/cpanel.config
-cp data/cpanel.config /var/cpanel/
-/usr/local/cpanel/whostmgr/bin/whostmgr2 -updatetweaksettings
-
