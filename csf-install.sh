5a410ac05d4f9bcdde63c63b5e27f45a92861c3d
Yujin Boby <admin@hostonnet.com>
2016-08-16 23:15:18 +0530

    updated csf install instruction

--- csf-install.sh
+++ csf-install.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-WORK_DIR="$( pwd )"
-
 cd /usr/local/src
 wget http://www.configserver.com/free/csf.tgz
 tar -xzf csf.tgz
 cd csf
 sh install.sh
-
-rm -f /etc/csf/csf.conf
-cp $WORK_DIR/data/csf.conf /etc/csf
-csf -r
