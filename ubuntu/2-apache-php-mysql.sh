99830289520d2cd012a5da1f50edcadd06dc870c
Yujin Boby <admin@hostonnet.com>
2017-01-19 13:06:12 +0530

    updated apache2 install instruction for ubuntu 16.04

--- ubuntu/2-apache-php-mysql.sh
+++ ubuntu/2-apache-php-mysql.sh
@@ -7,6 +7,11 @@ a2enmod rewrite
 apt-get -y install php php-cli php-curl php-gd php-mysql php-imagick php-imap php-mcrypt php-json
 phpenmod mcrypt
 
+apt-get -y install libapache2-mod-php
+
 apt-get -y install mariadb-client mariadb-server
 update-rc.d mysql enable
+
+service apache2 stop
+service apache2 start
 
