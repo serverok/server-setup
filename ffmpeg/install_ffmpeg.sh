c5dc768d198c7f3b0d2c2812cc38bcc55fce1a6a
Yujin Boby <admin@hostonnet.com>
2017-04-16 14:57:33 +0530

    added libvpx install instruction

--- ffmpeg/install_ffmpeg.sh
+++ ffmpeg/install_ffmpeg.sh
@@ -19,9 +19,9 @@ yum -y install libtool libcpp libgcc libstdc++
 yum -y install gcc4 gcc4-c++ gcc4-gfortran
 yum -y install gcc-c++ compat-gcc-32 compat-gcc-32-c++
 
-mkdir -p /usr/local/src/hostonnet-ffmpeg/tmp
-chmod 777 /usr/local/src/hostonnet-ffmpeg/tmp
-export TMPDIR=/usr/local/src/hostonnet-ffmpeg/tmp
+mkdir -p /usr/local/src/hostonnet/tmp
+chmod 777 /usr/local/src/hostonnet/tmp
+export TMPDIR=/usr/local/src/hostonnet/tmp
 
 if [ -f /usr/local/cpanel/version ];
 then
@@ -29,10 +29,10 @@ then
 else
     yum -y install ruby
     yum -y install freetype-devel
-    cd /usr/local/src/hostonnet-ffmpeg/
+    cd /usr/local/src/hostonnet/
     wget -c http://www.ijg.org/files/jpegsrc.v9a.tar.gz
     tar xvf jpegsrc.v9a.tar.gz
-    cd /usr/local/src/hostonnet-ffmpeg/jpeg-*
+    cd /usr/local/src/hostonnet/jpeg-*
     ./configure  --prefix=/usr
     make
     make install
@@ -40,10 +40,10 @@ fi
 
 # https://trac.ffmpeg.org/wiki/CompilationGuide/Ubuntu
 
-cd /usr/local/src/hostonnet-ffmpeg/
+cd /usr/local/src/hostonnet/
 wget -O fdk-aac.tar.gz https://github.com/mstorsjo/fdk-aac/tarball/master
 tar xzvf fdk-aac.tar.gz
-cd /usr/local/src/hostonnet-ffmpeg/mstorsjo-fdk-aac*
+cd /usr/local/src/hostonnet/mstorsjo-fdk-aac*
 autoreconf -fiv
 ./configure --prefix=/usr
 make
@@ -53,44 +53,44 @@ ldconfig
 
 # http://sourceforge.net/projects/faac/files/
 
-cd /usr/local/src/hostonnet-ffmpeg/
+cd /usr/local/src/hostonnet/
 wget -c http://downloads.sourceforge.net/project/faac/faac-src/faac-1.28/faac-1.28.tar.gz
 tar -zxvf faac-1.28.tar.gz
-cd /usr/local/src/hostonnet-ffmpeg/faac*
+cd /usr/local/src/hostonnet/faac*
 sed -i '/char \*strcasestr(const char \*haystack, const char \*needle);/d' ./common/mp4v2/mpeg4ip.h
 ./bootstrap
 ./configure --prefix=/usr
 make && make install
 ldconfig
 
-cd /usr/local/src/hostonnet-ffmpeg/
+cd /usr/local/src/hostonnet/
 wget -c http://downloads.sourceforge.net/project/faac/faad2-src/faad2-2.7/faad2-2.7.tar.gz
 tar -zxvf faad2-2.7.tar.gz
-cd /usr/local/src/hostonnet-ffmpeg/faad2*
+cd /usr/local/src/hostonnet/faad2*
 autoreconf -vif
 ./configure --prefix=/usr
 make && make install
 
 # https://xiph.org/downloads/
 
-cd /usr/local/src/hostonnet-ffmpeg/
+cd /usr/local/src/hostonnet/
 wget -c http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.gz
 tar zxvf libogg-1.3.2.tar.gz
-cd /usr/local/src/hostonnet-ffmpeg/libogg-1.3.2
+cd /usr/local/src/hostonnet/libogg-1.3.2
 ./configure && make && make install
 ldconfig
 
 # http://www.xiph.org/downloads/
 
-cd /usr/local/src/hostonnet-ffmpeg/
+cd /usr/local/src/hostonnet/
 wget -c http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.5.tar.gz
 tar zxvf libvorbis-1.3.5.tar.gz
-cd /usr/local/src/hostonnet-ffmpeg/libvorbis-1.3.5
+cd /usr/local/src/hostonnet/libvorbis-1.3.5
 ./configure && make && make install
 
 # http://www.xiph.org/downloads/
 
-cd /usr/local/src/hostonnet-ffmpeg/
+cd /usr/local/src/hostonnet/
 wget -c http://downloads.xiph.org/releases/theora/libtheora-1.1.1.tar.bz2
 tar xf libtheora-1.1.1.tar.bz2
 cd libtheora-1.1.1
@@ -99,82 +99,84 @@ make && make install
 
 # http://lame.sourceforge.net/download.php
 
-cd /usr/local/src/hostonnet-ffmpeg/
+cd /usr/local/src/hostonnet/
 wget -c http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz
 tar xf lame-3.99.5.tar.gz
-cd /usr/local/src/hostonnet-ffmpeg/lame-3.99.5
+cd /usr/local/src/hostonnet/lame-3.99.5
 ./configure --enable-shared --prefix=/usr
 make && make install
 
 # http://www.xvid.org/Downloads.43.0.html
 
-cd /usr/local/src/hostonnet-ffmpeg/
+cd /usr/local/src/hostonnet/
 wget -c http://downloads.xvid.org/downloads/xvidcore-1.3.4.tar.gz
 tar xvf xvidcore-1.3.4.tar.gz
-cd /usr/local/src/hostonnet-ffmpeg/xvidcore/build/generic/
+cd /usr/local/src/hostonnet/xvidcore/build/generic/
 ./configure --prefix=/usr && make && make install
 
 # http://rubyforge.org/projects/flvtool2/
 # http://www.inlet-media.de/2009/11/flvtool2.html
 
-cd /usr/local/src/hostonnet-ffmpeg/
+cd /usr/local/src/hostonnet/
 wget https://github.com/unnu/flvtool2/archive/master.zip -O flvtool2.zip
 unzip flvtool2.zip
-cd /usr/local/src/hostonnet-ffmpeg/flvtool2-master/
+cd /usr/local/src/hostonnet/flvtool2-master/
 ruby setup.rb config --prefix=/usr
 ruby setup.rb setup
 ruby setup.rb install
 
 # http://yamdi.sourceforge.net/
 
-cd /usr/local/src/hostonnet-ffmpeg/
+cd /usr/local/src/hostonnet/
 wget -c http://downloads.sourceforge.net/project/yamdi/yamdi/1.9/yamdi-1.9.tar.gz
 tar -zxvf yamdi-1.9.tar.gz
-cd /usr/local/src/hostonnet-ffmpeg/yamdi-1.9
+cd /usr/local/src/hostonnet/yamdi-1.9
 gcc yamdi.c -o yamdi -O2 -Wall
 mv yamdi /usr/bin/
 
 #cd /usr/local/src
 #git clone https://github.com/ioppermann/yamdi.git
 
-cd /usr/local/src/hostonnet-ffmpeg/
+cd /usr/local/src/hostonnet/
 wget -c http://liba52.sourceforge.net/files/a52dec-0.7.4.tar.gz
 tar xvf a52dec-0.7.4.tar.gz
-cd /usr/local/src/hostonnet-ffmpeg/a52dec*
+cd /usr/local/src/hostonnet/a52dec*
 ./configure
 make && make install
 
 # http://www.penguin.cz/~utx/amr#download
 
-cd /usr/local/src/hostonnet-ffmpeg/
+cd /usr/local/src/hostonnet/
 wget -c ftp://ftp.penguin.cz/pub/users/utx/amr/amrnb-11.0.0.0.tar.bz2
 tar jxvf amrnb-11.0.0.0.tar.bz2
-cd /usr/local/src/hostonnet-ffmpeg/amrnb-11.0.0.0
+cd /usr/local/src/hostonnet/amrnb-11.0.0.0
 ./configure
 make && make install
 
-cd /usr/local/src/hostonnet-ffmpeg/
+cd /usr/local/src/hostonnet/
 wget -c ftp://ftp.penguin.cz/pub/users/utx/amr/amrwb-11.0.0.0.tar.bz2
 tar jxvf amrwb-11.0.0.0.tar.bz2
-cd /usr/local/src/hostonnet-ffmpeg/amrwb-11.0.0.0
+cd /usr/local/src/hostonnet/amrwb-11.0.0.0
 ./configure
 make && make install
 
 # http://yasm.tortall.net/Download.html
 # 23-Oct-2015 09:01 AM
+# yasm --version
 
-cd /usr/local/src/hostonnet-ffmpeg/
+cd /usr/local/src/hostonnet/
 git clone git://github.com/yasm/yasm.git
-cd /usr/local/src/hostonnet-ffmpeg/yasm
+cd /usr/local/src/hostonnet/yasm
 ./autogen.sh
-./configure
+./configure --prefix=/usr
 make && make install
 
 # http://www.videolan.org/developers/x264.html
+# x264 --help
 
-cd /usr/local/src/hostonnet-ffmpeg/
+cd /usr/local/src/hostonnet/
 git clone git://git.videolan.org/x264.git
-cd /usr/local/src/hostonnet-ffmpeg/x264/
+cd /usr/local/src/hostonnet/x264/
 git pull
 make clean && make distclean
 ./configure --prefix=/usr --enable-shared
@@ -183,57 +185,57 @@ make && make install
 # 32bit versions needed for neroAacEnc
 yum -y install glibc.i686 libstdc++.i686
 
-cd /usr/local/src/hostonnet-ffmpeg/
+cd /usr/local/src/hostonnet/
 wget -c http://ftp6.nero.com/tools/NeroDigitalAudio.zip
 unzip NeroDigitalAudio.zip -d nero
-cd /usr/local/src/hostonnet-ffmpeg/nero/linux
+cd /usr/local/src/hostonnet/nero/linux
 install -D -m755 neroAacEnc /usr/bin
 
 # https://gpac.wp.mines-telecom.fr/
 # 23-Oct-2015 09:12 AM
 
-cd /usr/local/src/hostonnet-ffmpeg/
+cd /usr/local/src/hostonnet/
 wget https://github.com/gpac/gpac/archive/v0.6.1.tar.gz
 tar xf v0.6.1.tar.gz
-cd /usr/local/src/hostonnet-ffmpeg/gpac-0.6.1
+cd /usr/local/src/hostonnet/gpac-0.6.1
 ./configure --prefix=/usr
 make
 make install
 
 # https://mediaarea.net/en/MediaInfo/Download/Source
 
-cd /usr/local/src/hostonnet-ffmpeg/
+cd /usr/local/src/hostonnet/
 wget http://mediaarea.net/download/binary/mediainfo/0.7.84/MediaInfo_CLI_0.7.84_GNU_FromSource.tar.gz
 tar xf MediaInfo_CLI_0.7.84_GNU_FromSource.tar.gz
-cd /usr/local/src/hostonnet-ffmpeg/MediaInfo_CLI_GNU_FromSource
+cd /usr/local/src/hostonnet/MediaInfo_CLI_GNU_FromSource
 ./CLI_Compile.sh --prefix=/usr
-cd /usr/local/src/hostonnet-ffmpeg/MediaInfo_CLI_GNU_FromSource/MediaInfo/Project/GNU/CLI
+cd /usr/local/src/hostonnet/MediaInfo_CLI_GNU_FromSource/MediaInfo/Project/GNU/CLI
 make install
 
 # http://sourceforge.net/projects/libdc1394/files/?source=navbar
 # updated on 12-May-2015 09:32 AM
 
-cd /usr/local/src/hostonnet-ffmpeg/
+cd /usr/local/src/hostonnet/
 wget -c http://downloads.sourceforge.net/project/libdc1394/libdc1394-2/2.2.4/libdc1394-2.2.4.tar.gz
 tar zxfv libdc1394-2.2.4.tar.gz
-cd /usr/local/src/hostonnet-ffmpeg/libdc1394-2.2.4
+cd /usr/local/src/hostonnet/libdc1394-2.2.4
 make clean && make distclean
 ./configure
 make
 make install
 
-cd /usr/local/src/hostonnet-ffmpeg/
+cd /usr/local/src/hostonnet/
 wget http://www.mplayerhq.hu/MPlayer/releases/mplayer-export-snapshot.tar.bz2
 tar xvf mplayer-export-snapshot.tar.bz2
-cd /usr/local/src/hostonnet-ffmpeg/mplayer*
+cd /usr/local/src/hostonnet/mplayer*
 echo y | ./configure --prefix=/usr --codecsdir=/usr/local/lib/codecs/ --enable-theora
 make && make install
 
 
-cd /usr/local/src/hostonnet-ffmpeg/
+cd /usr/local/src/hostonnet/
 wget -c http://www.mplayerhq.hu/MPlayer/releases/codecs/all-20071007.tar.bz2
 tar -jxvf all-20071007.tar.bz2
-cd /usr/local/src/hostonnet-ffmpeg/all-20071007
+cd /usr/local/src/hostonnet/all-20071007
 mv /usr/local/lib/codecs /usr/local/lib/codecs-$(date +%m%d%Y%H%N)
 mkdir -p /usr/local/lib/codecs
 cp -f *.* /usr/local/lib/codecs/
@@ -244,20 +246,35 @@ ln -sf /usr/local/lib/codecs /usr/local/lib/win32
 ln -sf /usr/local/lib/codecs /usr/lib/win32
 ldconfig
 
+
+# https://www.webmproject.org/code/
+# https://github.com/webmproject/libvpx/releases
+# cmd = vpxenc
+
+cd /usr/local/src/hostonnet/
+wget https://github.com/webmproject/libvpx/archive/v1.6.1.tar.gz -O libvpx.tar.gz
+tar xvf libvpx.tar.gz
+cd /usr/local/src/hostonnet/libvpx-*
+make clean && make distclean
+./configure --prefix=/usr --enable-shared
+make
+make install
+
+
 # https://github.com/FFmpeg/FFmpeg/releases
 # Updated on 2016-10-13
 
-cd /usr/local/src/hostonnet-ffmpeg/
+cd /usr/local/src/hostonnet/
 # git clone git://git.videolan.org/ffmpeg.git
-# cd /usr/local/src/hostonnet-ffmpeg/ffmpeg
+# cd /usr/local/src/hostonnet/ffmpeg
 wget https://github.com/FFmpeg/FFmpeg/archive/n3.1.4.tar.gz
 tar zxf n3.1.4.tar.gz
-cd /usr/local/src/hostonnet-ffmpeg/FFmpeg-n3.1.4/
+cd /usr/local/src/hostonnet/FFmpeg-n3.1.4/
 make clean && make distclean
 ./configure --prefix=/usr --enable-shared --enable-libxvid --enable-libvorbis --enable-libtheora --enable-libmp3lame --enable-gpl --enable-libfdk-aac --enable-nonfree --enable-libx264 --enable-libfreetype
 make && make install && ldconfig
 
-cd /usr/local/src/hostonnet-ffmpeg/FFmpeg-n3.1.4/
+cd /usr/local/src/hostonnet/FFmpeg-n3.1.4/
 make tools/qt-faststart
 cp -a tools/qt-faststart /usr/bin/
 
