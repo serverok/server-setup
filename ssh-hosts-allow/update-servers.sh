712583d845edcba6f81021167de63638eeadf5db
Yujin Boby <admin@hostonnet.com>
2017-11-01 11:54:19 +0530

    updated hosts.allow ip. use proxychains to update servers

--- ssh-hosts-allow/update-servers.sh
+++ ssh-hosts-allow/update-servers.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-scp -P 3333 hosts.allow root@server20.hostonnet.com:/etc/hosts.allow
-scp -P 3333 hosts.deny root@server20.hostonnet.com:/etc/hosts.deny
+proxychains scp -P 3333 hosts.allow root@server20.hostonnet.com:/etc/hosts.allow
+proxychains scp -P 3333 hosts.deny root@server20.hostonnet.com:/etc/hosts.deny
 
-scp -P 3333 hosts.allow root@server46.hostonnet.com:/etc/hosts.allow
-scp -P 3333 hosts.deny root@server46.hostonnet.com:/etc/hosts.deny
+proxychains scp -P 3333 hosts.allow root@server46.hostonnet.com:/etc/hosts.allow
+proxychains scp -P 3333 hosts.deny root@server46.hostonnet.com:/etc/hosts.deny
 
-scp -P 3333 hosts.allow root@server48.hostonnet.com:/etc/hosts.allow
-scp -P 3333 hosts.deny root@server48.hostonnet.com:/etc/hosts.deny
+proxychains scp -P 3333 hosts.allow root@server48.hostonnet.com:/etc/hosts.allow
+proxychains scp -P 3333 hosts.deny root@server48.hostonnet.com:/etc/hosts.deny
 
-scp -P 3333 hosts.allow root@server74.hostonnet.com:/etc/hosts.allow
-scp -P 3333 hosts.deny root@server74.hostonnet.com:/etc/hosts.deny
+proxychains scp -P 3333 hosts.allow root@server74.hostonnet.com:/etc/hosts.allow
+proxychains scp -P 3333 hosts.deny root@server74.hostonnet.com:/etc/hosts.deny
 
-scp -P 3333 hosts.allow root@server78.hostonnet.com:/etc/hosts.allow
-scp -P 3333 hosts.deny root@server78.hostonnet.com:/etc/hosts.deny
+proxychains scp -P 3333 hosts.allow root@server78.hostonnet.com:/etc/hosts.allow
+proxychains scp -P 3333 hosts.deny root@server78.hostonnet.com:/etc/hosts.deny
 
-scp -P 3333 hosts.allow root@server22.hosthat.com:/etc/hosts.allow
-scp -P 3333 hosts.deny root@server22.hosthat.com:/etc/hosts.deny
+proxychains scp -P 3333 hosts.allow root@server22.hosthat.com:/etc/hosts.allow
+proxychains scp -P 3333 hosts.deny root@server22.hosthat.com:/etc/hosts.deny
