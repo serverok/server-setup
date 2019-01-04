e7a5bd2c97ec6939bb1a332c017b0cbe714aa394
ServerOk <admin@serverOK.in>
2019-01-05 00:45:01 +0530

    phpmyadmin version updated

--- install/phpmyadmin.sh
+++ install/phpmyadmin.sh
@@ -1,11 +1,11 @@
 # https://www.phpmyadmin.net/
 
 cd /usr/local/src
-wget https://files.phpmyadmin.net/phpMyAdmin/4.8.3/phpMyAdmin-4.8.3-all-languages.zip
-unzip phpMyAdmin-4.8.3-all-languages.zip
+wget https://files.phpmyadmin.net/phpMyAdmin/4.8.4/phpMyAdmin-4.8.4-all-languages.zip
+unzip phpMyAdmin-4.8.4-all-languages.zip
 mkdir /usr/serverok
 rm -rf /usr/serverok/phpmyadmin
-mv phpMyAdmin-4.8.3-all-languages /usr/serverok/phpmyadmin
+mv phpMyAdmin-4.8.4-all-languages /usr/serverok/phpmyadmin
 mkdir /usr/serverok/phpmyadmin/tmp/
 chmod 777 /usr/serverok/phpmyadmin/tmp/
 cp /usr/serverok/phpmyadmin/config.sample.inc.php /usr/serverok/phpmyadmin/config.inc.php
@@ -37,6 +37,7 @@ service httpd restart
 
 
 location /phpmyadmin {
+    client_max_body_size 100M;
     root /usr/serverok/;
     index index.php;
     location ~ ^/phpmyadmin/(.*\.php)$ {
