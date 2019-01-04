9d6a571d5f6ebb9a0c81039869d8348c1e36098e
ServerOk <admin@serverOK.in>
2019-01-05 00:36:46 +0530

    added letsencrypt renew script

---
+++ data/letsencrypt-renew.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Author: ServerOK.in
+# Email: admin@serverok.in
+# Web; https://www.serverok.in
+
+/usr/bin/certbot renew
+
+#systemctl restart apache2
+#systemctl restart httpd
+#systemctl restart nginx
+
