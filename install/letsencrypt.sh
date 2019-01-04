eedcce6f1300a51b4ce7c376823f3fcd51cc46ac
ServerOk <admin@serverOK.in>
2019-01-05 00:45:23 +0530

    letsencrypt use custom renew script

--- install/letsencrypt.sh
+++ install/letsencrypt.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Author: ServerOK.in
+# Email: admin@serverok.in
+# Web; https://www.serverok.in
 
 cd /usr/bin
 wget https://dl.eff.org/certbot-auto
@@ -6,8 +9,12 @@ chmod a+x /usr/bin/certbot-auto
 rm -f /usr/bin/certbot
 mv /usr/bin/certbot-auto /usr/bin/certbot
 
-certbot register --update-registration --email admin@serverok.in
+certbot register --agree-tos -m admin@serverok.in
 
 crontab -l > /root/crontab-backup
-echo "30 2 * * 1 /usr/bin/certbot renew >> /var/log/le-renew.log" >> /root/crontab-backup
+echo "30 2 * * 1 /usr/serverok/ssl-renew >> /var/log/le-renew.log" >> /root/crontab-backup
 crontab /root/crontab-backup
+
+mkdir /usr/serverok/
+wget https://raw.githubusercontent.com/serverok/server-setup/master/data/letsencrypt-renew.sh -O /usr/serverok/ssl-renew
+chmod 755 /usr/serverok/ssl-renew
