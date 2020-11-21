2550a5efda5431d598f7ad8bdfe73aea1330bb3a
ServerOk <admin@serverok.in>
2020-11-21 22:30:32 +0530

    updated mysqli test script

--- test-scripts/mysqli.php
+++ test-scripts/mysqli.php
@@ -1,7 +1,12 @@
 <?php
+# Script to test MySQL connection using mysqli
+# Author: ServerOk.in
+# Web: https://serverok.in
 
 $con = mysqli_connect("localhost","my_user","my_password","my_db");
 
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
+} else {
+    echo "Database connection sucessfull.";
 }
