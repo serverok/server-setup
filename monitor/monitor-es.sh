ffbd4a0c6f0c6776a2fd699455279e533b5aa7ac
ServerOk <admin@serverok.in>
2022-11-06 09:22:21 +0530

    updated elasticsearch monitor script

--- monitor/monitor-es.sh
+++ monitor/monitor-es.sh
@@ -2,8 +2,8 @@
 # Author: ServerOK
 # Web: https://serverok.in/how-to-auto-restart-elasticsearch-on-failure
 # Auto restart ElasticSearch on crash.
-# chmod 755 /usr/serverok/elasticsearch.sh
-# */5 * * * * /usr/serverok/elasticsearch.sh >> /var/log/sok-elasticsearch.log
+# chmod 755 /usr/serverok/monitor-es.sh
+# */5 * * * * /usr/serverok/monitor-es.sh >> /var/log/sok-es.log
 
 TIME_STAMP="$(date "+%Y-%m-%d %H:%M:%S")"
 CMD_RESULT="$(ps -ef | grep -v grep | grep elastic)"
